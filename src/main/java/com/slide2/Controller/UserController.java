352aa4a2ad46550118b821e5a08fc58962f80b85
tungrg <tungrg07@gmail.com>
2025-07-17 23:49:12 +0700

    add 3rd

---
+++ src/main/java/com/slide2/Controller/UserController.java
@@ -0,0 +1,8 @@
+package com.slide2.Controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UserController {
+
+}
--- src/main/java/com/slide2/service/UserService.java
+++ src/main/java/com/slide2/service/UserService.java
@@ -18,21 +18,46 @@ public class UserService {
     @PersistenceContext
     private EntityManager entityManager;
     
-    @Transactional
     public void saveUser(User user) {
-        entityManager.persist(user);
+        try {
+            if(getUserById(user.getId()) != null) {
+                entityManager.merge(user);
+                entityManager.getTransaction().commit();
+                System.out.println("User updated successfully!");
+            } else {
+                entityManager.persist(user);
+                entityManager.getTransaction().commit();
+                System.out.println("User created successfully!");
+            }
+        } catch (Exception e) {
+            System.err.println("Error saving user: " + e.getMessage());
+            entityManager.getTransaction().rollback();
+        }
         System.out.println("User saved successfully!");
     }
-    @Transactional
+
+
     public void deleteUser(User user) {
-        entityManager.remove(user);
-        System.out.println("User deleted successfully!");
+        try {
+            User existingUser = getUserById(user.getId());
+            if (existingUser != null) {
+                entityManager.remove(existingUser);
+                entityManager.getTransaction().commit();
+                System.out.println("User deleted successfully!");
+            } else {
+                System.out.println("User not found!");
+            }
+        } catch (Exception e) {
+            System.err.println("Error deleting user: " + e.getMessage());
+            entityManager.getTransaction().rollback();
+        }
     }
-    @Transactional
+
     public List<User> getAllUsers() {
         return entityManager.createQuery("SELECT u FROM User u", User.class).getResultList();
     }
-    @Transactional
+
+    
     public User getUserById(String id) {
         return entityManager.find(User.class, id);
     }
